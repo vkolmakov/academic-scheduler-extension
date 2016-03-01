7fb45eb82deb3ed2d00ae2887b656ed3886f0545
vkolmakov <vkolmakov@hotmail.com>
2016-03-01 16:00:45 -0600

    More refactoring: added a custom directive for inline form field.

--- js/ng-app/directives.js
+++ js/ng-app/directives.js
@@ -1,10 +1,20 @@
 app.directive('openSlotsTable', function () {
     return {
-        restrict: 'AE',
+        restrict: 'E',
         templateUrl: '../../views/templates/open-slots-table.html',
         scope: {
             tutorslist: '=',
             timetable: '='
         }
     };
-});
+})
+    .directive('formFieldInline', function () {
+        return {
+            transclude: true,
+            restrict: 'E',
+            templateUrl: '../../views/templates/form-field-inline.html',
+            scope: {
+                fieldName: '@'
+            }
+        };
+    });
